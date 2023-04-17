fcbea97bb87a463fc357d9ac7296b2c6fc7630e6
AbbasTestAccount <abbasfake5@gmail.com>
2023-04-18 01:36:27 +0330

    add the base of splash Activity and it's UI

--- app/src/main/java/ir/shabrangkala/simplelibrary/SplashActivity.kt
+++ app/src/main/java/ir/shabrangkala/simplelibrary/SplashActivity.kt
@@ -1,8 +1,11 @@
 package ir.shabrangkala.simplelibrary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ir.shabrangkala.simplelibrary.databinding.ActivitySplashBinding
+import java.util.Timer
+import kotlin.concurrent.timerTask
 
 lateinit var splashBinding: ActivitySplashBinding
 
@@ -11,5 +14,11 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         splashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
+
+        Timer().schedule(timerTask {
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
     }
 }
