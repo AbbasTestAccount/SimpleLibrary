daf3b195536b5966af3c35969faf5a3d039bef2a
AbbasTestAccount <abbasfake5@gmail.com>
2023-04-18 00:24:29 +0330

    add viewBinding to project

--- app/src/main/java/ir/shabrangkala/simplelibrary/MainActivity.kt
+++ app/src/main/java/ir/shabrangkala/simplelibrary/MainActivity.kt
@@ -2,10 +2,14 @@ package ir.shabrangkala.simplelibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ir.shabrangkala.simplelibrary.databinding.ActivityMainBinding
+
+lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
