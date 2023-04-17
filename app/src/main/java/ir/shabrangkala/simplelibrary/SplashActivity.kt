2487cce4aaafbb64542f7bd1b2b0eb98ca9b53b2
AbbasTestAccount <abbasfake5@gmail.com>
2023-04-18 02:00:43 +0330

    add random notes in splash

--- app/src/main/java/ir/shabrangkala/simplelibrary/SplashActivity.kt
+++ app/src/main/java/ir/shabrangkala/simplelibrary/SplashActivity.kt
@@ -6,14 +6,25 @@ import android.os.Bundle
 import ir.shabrangkala.simplelibrary.databinding.ActivitySplashBinding
 import java.util.Timer
 import kotlin.concurrent.timerTask
+import kotlin.collections.ArrayList
+import kotlin.random.Random
 
 lateinit var splashBinding: ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    val notes = arrayListOf<String>(" یک کتاب خوب، بهترین دوست و همدم انسان می باشد.",
+        "خانه بدون کتاب روز بدون خورشید است",
+        "یک کتاب، هزار نفر را می آموزد.",
+        " یک کتاب خوب، بهترین دوست و همدم انسان می باشد.",
+        "یک کتاب خوب روشن تر از ستاره ها میدرخشد.")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         splashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
+
+        splashBinding.note.text = notes[Random(System.nanoTime()).nextInt(0, notes.size)]
 
         Timer().schedule(timerTask {
             val intent = Intent(this@SplashActivity, MainActivity::class.java)
