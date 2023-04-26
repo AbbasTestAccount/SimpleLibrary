4d906897a3a8c523918e093cbf1bc20b1d9caed5
AbbasTestAccount <abbasfake5@gmail.com>
2023-04-27 01:05:02 +0330

    add glide to project

--- app/src/main/java/ir/shabrangkala/simplelibrary/fragments/HomeFragment.kt
+++ app/src/main/java/ir/shabrangkala/simplelibrary/fragments/HomeFragment.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import ir.shabrangkala.simplelibrary.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
@@ -17,5 +20,23 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //loading most viewed book's pictures
+        loadImageWithGlide(view,"https://newcdn.fidibo.com/images/books/99791_82810_normal.jpg?width=200" , binding.imageView1)
+        loadImageWithGlide(view,"https://newcdn.fidibo.com/images/books/99791_82810_normal.jpg?width=200" , binding.imageView2)
+        loadImageWithGlide(view,"https://newcdn.fidibo.com/images/books/99791_82810_normal.jpg?width=200" , binding.imageView3)
+        loadImageWithGlide(view,"https://newcdn.fidibo.com/images/books/99791_82810_normal.jpg?width=200" , binding.imageView4)
+        loadImageWithGlide(view,"https://newcdn.fidibo.com/images/books/99791_82810_normal.jpg?width=200" , binding.imageView5)
+
+
+
+
+    }
+
+    fun loadImageWithGlide(view : View, url: String, imageView: ImageView){
+        Glide.with(view)
+            .load(url)
+            .override(120, 180)
+            .into(imageView)
     }
 }
