b7a5d6e14fd64877a5c645af4c8dc8e73edb6d5b
AbbasTestAccount <abbasfake5@gmail.com>
2023-05-15 09:31:52 +0330

    add viewBinding to BookAdapter

--- app/src/main/java/ir/shabrangkala/simplelibrary/BookAdapter.kt
+++ app/src/main/java/ir/shabrangkala/simplelibrary/BookAdapter.kt
@@ -9,29 +9,31 @@ import android.widget.RatingBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import ir.shabrangkala.simplelibrary.databinding.BookItemBinding
 
 
 class BookAdapter(private val data : ArrayList<Book>):RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
+    lateinit var binding: BookItemBinding
 
     inner class BookViewHolder(itemView : View, private val context: Context): RecyclerView.ViewHolder(itemView){
-        val bookImage = itemView.findViewById<ImageView>(R.id.bookImage)
-        val bookName = itemView.findViewById<TextView>(R.id.bookName)
-        val authorName = itemView.findViewById<TextView>(R.id.authorName)
-        val numRate = itemView.findViewById<TextView>(R.id.numRate)
-        val price = itemView.findViewById<TextView>(R.id.price)
-        val rateBooks = itemView.findViewById<RatingBar>(R.id.rateBooks)
+//        val bookImage = itemView.findViewById<ImageView>(R.id.bookImage)
+//        val bookName = itemView.findViewById<TextView>(R.id.bookName)
+//        val authorName = itemView.findViewById<TextView>(R.id.authorName)
+//        val numRate = itemView.findViewById<TextView>(R.id.numRate)
+//        val price = itemView.findViewById<TextView>(R.id.price)
+//        val rateBooks = itemView.findViewById<RatingBar>(R.id.rateBooks)
 
         fun bindData(position: Int){
             Glide
                 .with(context)
                 .load(data[position].imgURL)
-                .into(bookImage)
+                .into(binding.bookImage)
 
-            bookName.text = data[position].bookName
-            authorName.text = data[position].authorName
-            numRate.text = data[position].numRate.toString()
-            price.text = data[position].price
-            rateBooks.rating = data[position].rateBooks
+            binding.bookName.text = data[position].bookName
+            binding.authorName.text = data[position].authorName
+            binding.numRate.text = data[position].numRate.toString()
+            binding.price.text = data[position].price
+            binding.rateBooks.rating = data[position].rateBooks
 
 
         }
@@ -39,8 +41,10 @@ class BookAdapter(private val data : ArrayList<Book>):RecyclerView.Adapter<BookA
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.book_item, parent, false)
-        return BookViewHolder(view, parent.context)
+        binding = BookItemBinding.inflate(LayoutInflater.from(parent.context),parent, false)
+        return BookViewHolder(binding.root, parent.context)
+        // val view = LayoutInflater.from(parent.context).inflate(R.layout.book_item, parent, false)
+        //return BookViewHolder(view, parent.context)
     }
 
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
